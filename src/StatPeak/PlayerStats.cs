fe7c25ed96902dfff14a51bbf2bd2504aa067ada
Kirsho <mufuz328@gmail.com>
2025-08-27 18:05:26 +0200

    Add Affliction patches
    By applying one patch to AddStatus we are able to monitor all
    afflictions that are applied to the character.

---
+++ src/StatPeak/PlayerStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace StatPeak;
+
+internal class PlayerStats
+{
+    private readonly static Dictionary<string, double> stats = new(2);
+        
+    public static void Increment(string name, double amount = 1)
+    {
+        if (!stats.TryGetValue(name, out double current))
+        {
+            Set(name, amount);
+            return;
+        }
+
+        stats[name] = current + amount;
+    }
+
+    public static void Set(string name, double value)
+    {
+        if (!stats.ContainsKey(name))
+        {
+            stats.Add(name, value);
+            return;
+        }
+
+        stats[name] = value;
+    }
+
+    public static double Get(string name)
+    {
+        return stats[name];
+    }
+
+    public static void Reset()
+    {
+        stats.Clear();
+    }
+}
--- src/StatPeak/Plugin.cs
+++ src/StatPeak/Plugin.cs
@@ -1,13 +1,16 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
+using HarmonyLib;
 
 namespace StatPeak;
 
 [BepInAutoPlugin]
 public partial class Plugin : BaseUnityPlugin
 {
-    internal static ManualLogSource Log { get; private set; } = null!;
+    internal new static ManualLogSource Logger;
+    private readonly Harmony _harmony = new(Id);
 
     internal static ConfigEntry<string> RemoteServerBaseURL { get; private set; }
     internal static ConfigEntry<string> RemoteServerBasePath { get; private set; }
@@ -15,8 +18,11 @@ public partial class Plugin : BaseUnityPlugin
 
     private void Awake()
     {
-        Log = base.Logger;
-        Log.LogInfo($"Plugin {Name} is loaded!");
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {Name} is loaded!");
+
+        _harmony.PatchAll(typeof(Plugin.AfflictionPatch));
+        Logger.LogInfo($"All affliction patches applied successfully.");
 
         RemoteServerBaseURL = Config.Bind(
             "RemoteServer",
@@ -39,4 +45,46 @@ public partial class Plugin : BaseUnityPlugin
             "Toggle to specify whether server communicates with HTTP or HTTPS protocol. 'true' signifies HTTPS, 'false' - HTTP"
         );
     }
+
+    public class AfflictionPatch
+    {
+        private static CharacterAfflictions.STATUSTYPE lastAffliction = CharacterAfflictions.STATUSTYPE.Hunger;
+        private static float AccumulatedAmount = 0;
+        private static DateTime lastLogTime = DateTime.MinValue;
+
+        [HarmonyPatch(typeof(CharacterAfflictions), nameof(CharacterAfflictions.AddStatus))]
+        [HarmonyPostfix]
+        public static void IncrementStatus(ref CharacterAfflictions __instance, ref bool __result, CharacterAfflictions.STATUSTYPE statusType, float amount)
+        {
+            if (!__result)
+            {
+                // Ignore calls to AddStatus if they didn't add status to character
+                return;
+            }
+
+            if (!__instance.character.IsLocal)
+            {
+                // Ignore calls that are not about local player
+                return;
+            }
+
+            // Add before reseting to not lose information about last amount
+            AccumulatedAmount += amount;
+            PlayerStats.Increment(statusType.ToString().ToLower(), amount);
+
+            if (lastAffliction != statusType || DateTime.Now > lastLogTime.AddMinutes(1))
+            {
+                // Only log when new affliction type has called AddStatus
+                // or when you are accumulating for more than 1 minute
+                //
+                // Otherwise, will flood console with lots of "Added X.XXXXXX of hunger"
+                Plugin.Logger.LogDebug($"Adding {AccumulatedAmount} of {lastAffliction.ToString().ToLower()}");
+
+                lastLogTime = DateTime.Now;
+                AccumulatedAmount = 0;
+                lastAffliction = statusType;
+            }
+        }
+    }
+
 }
