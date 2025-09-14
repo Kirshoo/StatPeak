2a00b873fa627a4b1d05f4eb5c87f92fbe600052
Kirsho <mufuz328@gmail.com>
2025-09-14 12:58:44 +0200

    Add scout cannon use tracking

---
+++ src/StatPeak/Patches/ItemUsePatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace StatPeak.Patches;
+
+public class ItemUsePatch
+{
+    [HarmonyPatch(typeof(ScoutCannonFuse), nameof(ScoutCannonFuse.Interact_CastFinished))]
+    [HarmonyPostfix]
+    public static void IncrementCannonUses(Character interactor)
+    {
+        // Ignore non local character
+        if (!interactor.IsLocal)
+        {
+            return;
+        }
+
+        Plugin.Logger.LogDebug("Local player lit the fuse. Incrementing scout cannon uses.");
+        PlayerStats.Increment(ItemName.ScoutCannon);
+    }
+}
